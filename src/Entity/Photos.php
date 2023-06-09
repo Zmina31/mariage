88306afcb3d78073898aa76e709de48fe498f43b
Mina ZEKRI <mina.zekri2022@campus-eni.fr>
2023-06-09 09:40:59 +0200

    photosRepository

---
+++ src/Entity/Photos.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PhotosRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: PhotosRepository::class)]
+class Photos
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $nom = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $cheminImg = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): self
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getCheminImg(): ?string
+    {
+        return 'img/posters/' .$this->cheminImg;
+    }
+
+    public function setCheminImg(string $cheminImg): self
+    {
+        $this->cheminImg = $cheminImg;
+
+        return $this;
+    }
+}
